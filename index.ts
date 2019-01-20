5556f66db5b23d3f590a9ff2d228e132c30cbd25
Hodaka Suzuki <altitude3190@gmail.com>
2019-01-20 23:48:51 +0900

    cli arguments

--- index.ts
+++ index.ts
@@ -1,8 +1,19 @@
 class Startup {
-    public static main(): number {
-        console.log("Hello World");
-        return 0;
+    public static main(hoge: string): number {
+        console.log('Hello World' + hoge)
+        return 0
     }
 }
 
-Startup.main();
+function today(): string {
+    const today = new Date()
+    const YYYY = today.getFullYear()
+    const MM = `0${today.getMonth() + 1}`.slice(-2)
+    const DD = `0${today.getDate()}`.slice(-2)
+    return `${YYYY}-${MM}-${DD}`
+}
+
+const project = process.argv[2]
+const since = process.argv[3] || today()
+
+Startup.main(since)
